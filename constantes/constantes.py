610ccb67a8c2bb6717aded8465fb8edc7aa094ac
Damián <grimaldidamian001@gmail.com>
2024-07-21 21:47:51 -0300

    Dibujo del boton de dificultad

--- constantes/constantes.py
+++ constantes/constantes.py
@@ -1,2 +1,8 @@
 ancho = 1280
 alto = 720
+centro_ancho = ancho//2
+centro_alto = alto//2
+ancho_Rect = 200
+alto_Rect = 100
+tamaño_letra = 25
+colores = ["green","blue","red"]
--- pantalla/screen.py
+++ pantalla/screen.py
@@ -10,19 +10,42 @@ class screen():
         self.fondo = pygame.image.load("RECURSOS/fondo.jpg")
         self.etapa = "menu"
         self.running = True
+        self.myFond = pygame.font.SysFont('Times New Roman', tamaño_letra)
         self.reloj = pygame.time.Clock()
+        self.dificultad = ["FACIL","MEDIO","DIFICIL"]
+        self.inicio = 0
 
     def manejo_evento(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    self.running = False
+                if self.etapa != "menu":
+                    if event.key == pygame.K_ESCAPE:
+                        self.etapa = "menu"
+                else:
+                    if event.key == pygame.K_RIGHT:
+                        self.inicio +=1
+                        if self. inicio == 3:
+                            self.inicio = 0
+                    elif event.key == pygame.K_LEFT:
+                        self.inicio -= 1
+                        if self.inicio ==-1:
+                            self.inicio = 2
+
+    def dibujo_Texto(self,texto,posicion_x,posicion_y):
+        ancho_texto,_ = self.myFond.size(texto)
+        texto = self.myFond.render(texto,True,(0,0,0))
+        self.screen.blit(texto,(posicion_x - (ancho_texto//2),posicion_y))
+
+    def dibujar_menu(self):
+        pygame.draw.rect(self.screen,colores[self.inicio],(centro_ancho-(ancho_Rect//2),centro_alto-(alto_Rect//2),ancho_Rect,alto_Rect))
+        self.dibujo_Texto(self.dificultad[self.inicio],centro_ancho,centro_alto - (tamaño_letra//2))
 
     def dibujar(self):
         if self.etapa == "menu":
             self.screen.blit(self.fondo,(0,0))
+            self.dibujar_menu()
         
         pygame.display.flip()
 
